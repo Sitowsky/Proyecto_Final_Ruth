6f14bd5005698624832415609f7396dcb12ab34a
chuy_ <chuy_@HERRERAP>
2020-10-10 15:04:26 -0500

    5mentarios :v otra vez He hecho la clase table generica , ahora puede imprimir ambas tablas de practica 1 y 2 Ya obtenemos las expresiones del programa

--- Anlizador_Lexico_Sintactico/src/analizador_sintactico/Arbol_Sintactico.java
+++ Anlizador_Lexico_Sintactico/src/analizador_sintactico/Arbol_Sintactico.java
@@ -32,19 +32,21 @@ public class Arbol_Sintactico {
 
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
-     public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
-
+    
+//tokens es la matriz de la tabla de tokens , simbols la matriz de la segunda tabla de simboloes , 
+    //simbols table es la misma de la segunda tabla pero en arraylist 
+    
     public Arbol_Sintactico(String[][] tokens, String[][] simbols, ArrayList<String> simbols_table) {
         System.out.println("\n*******************************************************");
         System.out.println("\nArbol sintactico abstracto\n");
         Expresion(tokens, simbols_table);
 
-    
     }
 
     public void Expresion(String[][] tokens, ArrayList<String> simbols_table) {// construiremos toda nuestra linea de expresion 
-       
+
         String expresion = "";
         int desde = 0;
         for (int fila = 0; fila < tokens.length; fila++) {
@@ -70,7 +72,7 @@ public class Arbol_Sintactico {
         }
 
     }
-
+//
     public void Validacion(String[][] tokens, ArrayList<String> simbols_table, String expresion, int desde, int hasta) {
 
         int n = 0;
@@ -79,41 +81,45 @@ public class Arbol_Sintactico {
         for (int i = desde + 1; i < hasta; i++) {
             if (tokens[i][2].equals("Int") || tokens[i][2].equals("Flotante")) {
                 variables[n] = tokens[i][2];
+                System.out.print(ANSI_RED + "   " + variables[n] +" "+ tokens[i][0]+"  |"+ ANSI_RESET);
                 n++;
             }
         }
 
         String[] e = expresion.split("\\:");
-        String tipeFinal = "Int";  
+        String tipeFinal = "";
         for (int i = 0; i < variables.length; i++) {
-            tipeFinal = comprobacionTipos(tipeFinal, variables[i]);
-            System.out.print("   "+tipeFinal +" |");
-        }
-        
-//        System.out.println(ANSI_RED + "Texto de color rojo" + ANSI_RESET);
-        System.out.println(ANSI_BLUE+"\n"+e[0]+" es tipo " + tipeFinal+ANSI_RESET);
-        System.out.println("---------------------------------------" );
-        
-        
-        
+            if (i == 0) {
+                tipeFinal = variables[i];//el primer termino sera nuestro valor final , dado que es el primero
 
-        //Ahora need agregar a la tabla esos valores 
+            } else {
+                System.out.print("\n("+tipeFinal +") ("+variables [i]+") " );
+                tipeFinal = comprobacionTipos(tipeFinal, variables[i]);
+                System.out.print("---> (" + tipeFinal+")" );
+            }
+            
+        }
+
+//        System.out.println(ANSI_RED + "Texto de color rojo" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "\n" + e[0] + " es tipo " + tipeFinal + ANSI_RESET);
+        System.out.println("---------------------------------------");
+
+        //Ahora need agregar a la tabla esos valores AQUI FABRICIOOOOOOOOOO, la tabla se llama  simbols_table , ahi viene la 
+        //tabla de la practica dos , aqui la puedes usar , en esta clase la pase como parametro 
+        
     }
 
-//    private void numSubExpr(String expresion) {
-//      
-////        System.out.println("la expresion del split -----" + e[1]);
-//    }
+
     private String comprobacionTipos(String tipeFinal, String tipo) { //comprobar el tipo final de la expresion
 
         if (tipeFinal.equals("Int") && tipo.equals("Int")) { //si ambos son int
             return "Int";
         } else if (tipeFinal.equals("Int") && tipo.equals("Flotante")) {
-            return "Flotante"; //si ya es un flotante , debe ser un flotante o "Real"
+            return "Flotante"; 
         } else if (tipeFinal.equals("Flotante") && tipo.equals("Int")) {
-            return "Flotante"; //si ya es un flotante , debe ser un flotante o "Real"
+            return "Flotante"; 
         } else if (tipeFinal.equals("Flotante") && tipo.equals("Flotante")) {
-            return "Flotante"; //si ya es un flotante , debe ser un flotante o "Real"
+            return "Flotante";
         }
         return null;
 
