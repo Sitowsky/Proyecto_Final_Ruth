1526adf3449b45c03cebf4c87a6ba89581129784
chuy_ <chuy_@HERRERAP>
2020-10-30 20:09:23 -0600

    3 pesitos de comentarios :v

--- Anlizador_Lexico_Sintactico/src/analizador_sintactico/Conversión_Infija_PostFija.java
+++ Anlizador_Lexico_Sintactico/src/analizador_sintactico/Conversión_Infija_PostFija.java
@@ -16,46 +16,42 @@ public class Conversión_Infija_PostFija {
     
     @SuppressWarnings("empty-statement")
     public String infija_postfija(String g) {
-        ArrayList<precedencia> precede = new ArrayList();
+        ArrayList<precedencia> precede = new ArrayList();//nuestro arraylist de los operando posibles
         precede.add(new precedencia(4, "="));
         precede.add(new precedencia(1, "("));
         precede.add(new precedencia(3, "*"));
         precede.add(new precedencia(3, "/"));
         precede.add(new precedencia(2, "+"));
         precede.add(new precedencia(2, "-"));
-        Pila pilaOperadores = new Pila();
+        Pila pilaOperadores = new Pila(); //instanciamos una pila , para nuestros operadores 
         String sufija = "";
-        g=g.replace(":", "");
-        String[] simbols = g.split("");
+        g=g.replace(":", ""); //eliminamos el ":" de cada expresion
+        String[] simbols = g.split(""); //separamos nuestras expresion en caracteres para evaluarlos 
         for (String simbol : simbols) {
             if (("ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwqyz0123456789.").contains(simbol)) {
-
+                //si contiene algun operando la expresion se guardara en subfija
                 sufija = sufija + simbol;
 
-            } else if ("(".equals(simbol)) {
+            } else if ("(".equals(simbol)) { //si existe alguna subexpresion que se divida entre parentesis
                 pilaOperadores.push(simbol);
 
-            } else if (")".equals(simbol)) {
+            } else if (")".equals(simbol)) { //entraremos cuando se haya terminado alguna subexpresion expresa con parentesis 
 
-                String simboloTope = pilaOperadores.peek();
-                pilaOperadores.pop();
+                String simboloTope = pilaOperadores.peek(); //obtenemos el ultimo valor de la pila ")"
+                pilaOperadores.pop(); //lo retiramos (el ultimo elemento ingresado de la pila)
                 while (!"(".equals(simboloTope)) {
-                    sufija = sufija + simboloTope;
-                    simboloTope = pilaOperadores.peek();
-                    pilaOperadores.pop();
+                    sufija = sufija + simboloTope; //guardamos en nuestra expresion los operadores
+                    simboloTope = pilaOperadores.peek(); //obtenemos el siguiente operador de la pila
+                    pilaOperadores.pop();//retiramos el ultimos elemento añadido a la pila de operandos
                 }
 
             } else {
-
-
+                
                 while (!pilaOperadores.empty() && precdencia2(pilaOperadores, precede) >= precdencia(pilaOperadores,simbol, precede)) {
                     sufija = sufija + pilaOperadores.peek();
-
                     pilaOperadores.pop();
                 }
-
                 pilaOperadores.push(simbol);
-
             }
 
         }
@@ -84,7 +80,6 @@ public class Conversión_Infija_PostFija {
 
     private int precdencia2(Pila pilaOperadores, ArrayList<precedencia> precede) {
         if (!pilaOperadores.empty()) {
-
                     for (precedencia precede1 : precede) {
                         if (precede1.getSimbol().equals(pilaOperadores.peek())) {
                             return precede1.getKm();
