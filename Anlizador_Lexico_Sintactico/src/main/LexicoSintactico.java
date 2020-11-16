5e32a24e7f0e12b4d86221eaac00f92aca849252
chuy_ <chuy_@HERRERAP>
2020-11-15 19:04:12 -0600

    Medir el tiempo de ejecución

--- Anlizador_Lexico_Sintactico/src/main/LexicoSintactico.java
+++ Anlizador_Lexico_Sintactico/src/main/LexicoSintactico.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class LexicoSintactico {
 
     public static void main(String[] args)throws IOException {
+        //medir tiempo
+        long inicio = System.nanoTime();//variable de inicio time
         BufferedReader br = new BufferedReader(new FileReader("src/recursos/Cadena.txt")); //leer el programa a probar
         String line;
         String cadena = "";
@@ -18,6 +20,10 @@ public class LexicoSintactico {
         Analizador_Sintactico a = new Analizador_Sintactico(cadena);
         a.comenzar();
         
+        long fin = System.nanoTime();//variable de fin time
+        double tiempoEjecucion = (double)(fin - inicio) * 1.0e-9; //nanosegundos a segundos , 1nanos = 1e-9s
+        System.out.println("EL TIEMPO DE EJECUCION ES "+tiempoEjecucion);
+        
     }
-    
+ 
 }
